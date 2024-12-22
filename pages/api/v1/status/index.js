46e751216121684999ac237bef7c74c98bf29b05
Leandro Lima <llimasilva@proton.me>
2024-12-21 23:25:54 -0300

    add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,6 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;")
   response.status(200).json({
     chave: "Os alunos do curso.dev são fodas"
   })
